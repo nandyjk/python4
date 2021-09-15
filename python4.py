afd075b1ba9c14ad0926675bb5baa8d073ac2705
nandyjk <90789585+nandyjk@users.noreply.github.com>
2021-09-15 23:57:50 +0530

    created python4.py
    display different set operations

---
+++ python4.py
@@ -0,0 +1,15 @@
+#Set
+A = {0,1,2,3,4,5};
+B = {4,5,6,7,8,9};
+  
+# union
+print("Union :", A | B)
+  
+# intersection
+print("Intersection :", A & B)
+  
+# difference
+print("Difference :", A - B)
+  
+# symmetric difference
+print("Symmetric difference :", A ^ B)
